a39207ef510d470d88a1867c9876dbc4331f41c8
嘉文 史 <393575404@qq.com>
2025-06-29 15:52:55 +0800

    Add basic neuron.

---
+++ NeuralNetwork/Neurons/Neuron.cs
@@ -0,0 +1,9 @@
+﻿namespace NeuralNetwork.Neurons;
+
+public class ForwardContext;
+
+public abstract class Neuron
+{
+    public abstract double Output { get; }
+    public abstract void Forward(ForwardContext context);
+}
