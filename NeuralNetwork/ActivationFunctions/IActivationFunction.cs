c1a9fad02d1e93f2abf3189fcb57c4e09ef3ccf6
嘉文 史 <393575404@qq.com>
2025-06-30 23:06:35 +0800

    Remove redundant interface ISingleActivationFunction.

--- NeuralNetwork/ActivationFunctions/IActivationFunction.cs
+++ NeuralNetwork/ActivationFunctions/IActivationFunction.cs
@@ -5,12 +5,3 @@ public partial interface IActivationFunction
     static abstract Vector Apply(Vector vector);
     static abstract Vector Derivative(Vector vector);
 }
-
-public interface ISingleActivationFunction<TSelf> : IActivationFunction where TSelf : ISingleActivationFunction<TSelf>
-{
-    static abstract double Apply(double x);
-    static abstract double Derivative(double x);
-
-    static Vector IActivationFunction.Apply(Vector v) => v.Map(static x => TSelf.Apply(x));
-    static Vector IActivationFunction.Derivative(Vector v) => v.Map(static x => TSelf.Derivative(x));
-}
